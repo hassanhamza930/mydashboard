29362cba1ecf13e7ab7bffa548837b2c35cf214d
huzaifa-saleem <huzaifa.saleem3111@gmail.com>
2023-09-16 19:19:52 +0500

    mac redirection added

--- electron/main.ts
+++ electron/main.ts
@@ -72,19 +72,8 @@ const gotTheLock = app.requestSingleInstanceLock();
 if (!gotTheLock) {
   app.quit();
 } else {
-
   app.on("second-instance", (event, commandLine, workingDirectory) => {
     const uid = commandLine?.find((e) => e.includes("authuid"))?.split("=")[1];
-
-    const googleToken = commandLine
-      ?.find((e) => e.includes("oauthIdToken"))
-      ?.split("=")[1];
-    const facebookToken = commandLine
-      ?.find((e) => e.includes("facebookIdToken"))
-      ?.split("=")[1];
-    const microsoftToken = commandLine
-      ?.find((e) => e.includes("microsoftIdToken"))
-      ?.split("=")[1];
 
     if (win) {
       if (win.isMinimized()) win.restore();
@@ -97,38 +86,28 @@ if (!gotTheLock) {
       win.webContents.send("uid", uid);
       return;
     }
-    if (facebookToken) {
-      console.log("facebookAuthIdToken");
-      ipcMain.emit("facebookAuthIdToken", facebookToken);
-      win.webContents.send("facebookAuthIdToken", facebookToken);
-      return;
-    } else if (microsoftToken) {
-      console.log("microsoftAuthIdToken");
-      ipcMain.emit("microsoftAuthIdToken", microsoftToken);
-      win.webContents.send("microsoftAuthIdToken", microsoftToken);
-      return;
-    }
-    console.log("oauthIdToken");
-    ipcMain.emit("oauthIdToken", googleToken);
-    win.webContents.send("oauthIdToken", googleToken);
-  });
-
-
-
-  app.on("open-url", (event, url) => {
-    console.log("Received custom protocol URL:", url);
-    const token = new URL(url).searchParams.get("authuid");
-    console.log("OAuth ID Token:", token);
-    win.webContents.send("uid", token);
-    ipcMain.emit("uid", token);
-
-    // ...
-  });
-
-  app.on("window-all-closed", () => {
-    win = null;
   });
 }
+app.on("open-url", (event, url) => {
+  const uid = url?.split("=")[1];
+
+  if (win) {
+    if (win.isMinimized()) win.restore();
+    win.focus();
+  }
+
+  if (uid) {
+    console.log("uid", uid);
+    ipcMain.emit("uid", uid);
+    win.webContents.send("uid", uid);
+    return;
+  }
+});
+
+app.on("window-all-closed", () => {
+  win = null;
+});
+
 app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
